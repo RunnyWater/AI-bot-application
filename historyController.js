09931fcdb82d0e74477cc040954e6837064515f2
alekr0n <misharom185@gmail.com>
2024-03-20 15:53:50 +0100

    Add function for work with history

--- historyController.js
+++ historyController.js
@@ -1,2 +1,35 @@
-// Функция для добавления запроса и ответа в историю
-// ? Какие функции еще нужны
+
+// Загрузка всей истории из файла
+function loadHistory(){
+    const data = fs.readFileSync('history.json', 'utf8');
+    return JSON.parse(data);
+}
+
+// Загрузка истории пользователя из файла
+function loadUserHistory(userId){
+    let userData = loadHistory();
+    return userData[userId];
+}
+
+// Сохранение всей истории в файл
+function saveHistory(history){
+    const data = JSON.stringify(history);
+    fs.writeFileSync('history.json', data);
+}
+
+// Добавление нового вопроса в историю
+function addToUserHistory(userId, query, response) {
+    const history = loadHistory();
+    let userHistory = history[userId];
+    userHistory.push(createObject(query, response));
+    saveHistory(history);
+}
+
+// Создание объекта вопрос с ответами.
+function createObject(query, responses) {
+    return {
+        "query": query,
+        "response": responses,
+        "id": genId()
+    };
+}
